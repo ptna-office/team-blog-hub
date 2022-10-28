c8ebc7640bccd89e81201ad61999d97ed191e2ec
HoskaKeigo <gomagoma313@gmail.com>
2022-10-28 18:14:57 +0900

    add CTO to members.ts

--- members.ts
+++ members.ts
@@ -11,4 +11,15 @@ export const members: Member[] = [
     twitterUsername: "ptna_web",
     websiteUrl: "https://corporate.piano.or.jp/ptnyan/index.html",
   },
+  {
+    id: "11",
+    name: "Hiroyuki Noguchi",
+    role: "CTO",
+    bio: "ピティナ（一般社団法人全日本ピアノ指導者協会） 本部CTO／株式会社東音企画 外部 CTO。",
+    avatarSrc: "/avatars/hi-noguchi.png",
+    sources: ["https://qiita.com/Hi_Noguchi/feed","https://zenn.dev/hi_noguchi/feed"],
+    twitterUsername: "Hi_Noguchi",
+    githubUsername:"hi-noguchi",
+    websiteUrl: "https://hi-noguchi.com/"
+  }
 ];
