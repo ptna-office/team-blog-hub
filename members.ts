0df1c648d83ffe622964f034b657ef289aec62e0
HoskaKeigo <gomagoma313@gmail.com>
2022-10-28 18:38:35 +0900

    add Victor to member.ts

--- members.ts
+++ members.ts
@@ -17,9 +17,19 @@ export const members: Member[] = [
     role: "CTO",
     bio: "ピティナ（一般社団法人全日本ピアノ指導者協会） 本部CTO／株式会社東音企画 外部 CTO。",
     avatarSrc: "/avatars/hi-noguchi.png",
-    sources: ["https://qiita.com/Hi_Noguchi/feed","https://zenn.dev/hi_noguchi/feed"],
+    sources: ["https://qiita.com/Hi_Noguchi/feed", "https://zenn.dev/hi_noguchi/feed"],
     twitterUsername: "Hi_Noguchi",
-    githubUsername:"hi-noguchi",
-    websiteUrl: "https://hi-noguchi.com/"
-  }
+    githubUsername: "hi-noguchi",
+    websiteUrl: "https://hi-noguchi.com/",
+  },
+  {
+    id: "90",
+    name: "Victor Manuel",
+    role: "Flutter app developer",
+    bio: "Mexican, living in Japan Tokyo. I'm iOS, Android developer. メキシコ人日本に住んでいました。エンジニア iOS + Flutter + Android, 納豆が好きです。",
+    avatarSrc: "/avatars/vicor_emanuel.png",
+    sources: ["https://medium.com/feed/@victormanuellagunas"],
+    twitterUsername: "VicktorManuel",
+    githubUsername: "victormanuelfrancodev",
+  },
 ];
