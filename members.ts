d26efc93177558b0b6bfa50637cb99c50474cc5b
HosakaKeigo <74914629+HosakaKeigo@users.noreply.github.com>
2022-12-12 11:28:05 +0900

    remove ぴてぃにゃん

--- members.ts
+++ members.ts
@@ -2,16 +2,6 @@ import { Member } from "@src/types";
 
 export const members: Member[] = [
   {
-    id: "ptnyan",
-    name: "ぴてぃにゃん",
-    role: "mascot",
-    bio: "ぴてぃにゃん公式キャラクター",
-    avatarSrc: "/avatars/ptnyan.jpg",
-    sources: ["https://www.piano.or.jp/atom.xml"],
-    twitterUsername: "ptna_web",
-    websiteUrl: "https://corporate.piano.or.jp/ptnyan/index.html",
-  },
-  {
     id: "hi-noguchi",
     name: "Hiroyuki Noguchi",
     role: "CTO",
